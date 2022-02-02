1cc827306baa1708ff0a4cc6ff8d1e3e7e955fe7
Vipul <vipulrawat008@gmail.com>
2022-02-02 17:44:54 +0530

    feature: Added http/delivery layer

---
+++ delivery/product/http.go
@@ -0,0 +1,35 @@
+package product
+
+import (
+	"gofrPractice/service"
+	"strconv"
+
+	"developer.zopsmart.com/go/gofr/pkg/errors"
+	"developer.zopsmart.com/go/gofr/pkg/gofr"
+)
+
+type handler struct {
+	service service.Services
+}
+
+func New(s service.Services) handler {
+	return handler{
+		service: s,
+	}
+}
+
+func (h *handler) GetById(ctx *gofr.Context) (interface{}, error) {
+	i := ctx.PathParam("id")
+
+	if i == "" {
+		return nil, errors.MissingParam{Param: []string{"id"}}
+	}
+
+	id, err := strconv.Atoi(i)
+	if err != nil {
+		return nil, errors.InvalidParam{Param: []string{"id"}}
+	}
+
+	resp, err := h.service.GetById(ctx, id)
+	return resp, err
+}
---
+++ delivery/product/http_test.go
@@ -0,0 +1,117 @@
+package product
+
+import (
+	"gofrPractice/models"
+	"gofrPractice/service"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"developer.zopsmart.com/go/gofr/pkg/errors"
+	"developer.zopsmart.com/go/gofr/pkg/gofr"
+	"developer.zopsmart.com/go/gofr/pkg/gofr/request"
+	"developer.zopsmart.com/go/gofr/pkg/gofr/responder"
+	"github.com/golang/mock/gomock"
+)
+
+func TestGetById(t *testing.T) {
+	app := gofr.New()
+	ctrl := gomock.NewController(t)
+	mock := service.NewMockServices(ctrl)
+	h := New(mock)
+
+	testCases := []struct {
+		desc   string
+		input  string
+		calls  []*gomock.Call
+		resp   *models.Product
+		expErr error
+	}{
+		{
+			desc:  "success case",
+			input: "1",
+			resp: &models.Product{
+				Id:   1,
+				Name: "test",
+				Type: "example",
+			},
+			calls: []*gomock.Call{
+				mock.EXPECT().GetById(gomock.Any(), 1).Return(&models.Product{
+					Id:   1,
+					Name: "test",
+					Type: "example",
+				}, nil),
+			},
+			expErr: nil,
+		},
+		{
+			desc:  "id not in database",
+			input: "102",
+			resp:  nil,
+			expErr: errors.EntityNotFound{
+				Entity: "product",
+				ID:     "102",
+			},
+			calls: []*gomock.Call{
+				mock.EXPECT().GetById(gomock.Any(), 102).Return(nil, errors.EntityNotFound{
+					Entity: "product",
+					ID:     "102",
+				}),
+			},
+		},
+		{
+			desc:  "invalid id",
+			input: "-1",
+			resp:  nil,
+			expErr: errors.InvalidParam{
+				Param: []string{"id"},
+			},
+			calls: []*gomock.Call{
+				mock.EXPECT().GetById(gomock.Any(), -1).Return(nil, errors.InvalidParam{
+					Param: []string{"id"},
+				}),
+			},
+		},
+		{
+			desc:  "invalid id",
+			input: "abc",
+			resp:  nil,
+			expErr: errors.InvalidParam{
+				Param: []string{"id"},
+			},
+		},
+		{
+			desc:  "missing params",
+			input: "",
+			resp:  nil,
+			expErr: errors.MissingParam{
+				Param: []string{"id"},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "http://dummy", nil)
+
+			req := request.NewHTTPRequest(r)
+			res := responder.NewContextualResponder(w, r)
+
+			ctx := gofr.NewContext(res, req, app)
+
+			ctx.SetPathParams(map[string]string{
+				"id": tc.input,
+			})
+
+			resp, err := h.GetById(ctx)
+			if !reflect.DeepEqual(err, tc.expErr) {
+				t.Errorf("%s : expected %v, but got %v", tc.desc, tc.expErr, err)
+			}
+			if tc.expErr == nil && !reflect.DeepEqual(resp, tc.resp) {
+				t.Errorf("%s : expected %v, but got %v", tc.desc, tc.resp, resp)
+			}
+		})
+	}
+}
