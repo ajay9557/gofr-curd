968491992fb40e7a364629b6591e80e8aac9a0ac
Vipul <vipulrawat008@gmail.com>
2022-02-07 09:57:36 +0530

    Adding delivery layer and response structure

--- delivery/product/http.go
+++ delivery/product/http.go
@@ -1,7 +1,9 @@
 package product
 
 import (
+	"gofr-curd/models"
 	"gofr-curd/service"
+	"net/http"
 	"strconv"
 
 	"developer.zopsmart.com/go/gofr/pkg/errors"
@@ -31,5 +33,79 @@ func (h *handler) GetById(ctx *gofr.Context) (interface{}, error) {
 	}
 
 	resp, err := h.service.GetById(ctx, id)
+	return &models.Response{
+		Data:       *resp,
+		Message:    "data retrieved",
+		StatusCode: http.StatusOK,
+	}, err
+}
+
+func (h *handler) Get(ctx *gofr.Context) (interface{}, error) {
+	resp, err := h.service.Get(ctx)
 	return resp, err
+}
+
+func (h *handler) Create(ctx *gofr.Context) (interface{}, error) {
+	var p models.Product
+	if err := ctx.Bind(&p); err != nil {
+		ctx.Logger.Errorf("error in binding: %v", err)
+		return nil, errors.InvalidParam{Param: []string{"body"}}
+	}
+
+	if p.Id != 0 {
+		return nil, errors.InvalidParam{Param: []string{"id"}}
+	}
+
+	resp, err := h.service.Create(ctx, p)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+func (h *handler) Update(ctx *gofr.Context) (interface{}, error) {
+	i := ctx.PathParam("id")
+
+	if i == "" {
+		return nil, errors.MissingParam{Param: []string{"id"}}
+	}
+
+	id, err := strconv.Atoi(i)
+	if err != nil {
+		return nil, errors.InvalidParam{Param: []string{"id"}}
+	}
+
+	var p models.Product
+	if err := ctx.Bind(&p); err != nil {
+		ctx.Logger.Errorf("error in binding: %v", err)
+		return nil, errors.InvalidParam{Param: []string{"body"}}
+	}
+	p.Id = id
+	resp, err := h.service.Update(ctx, p)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func (h *handler) Delete(ctx *gofr.Context) (interface{}, error) {
+	i := ctx.PathParam("id")
+
+	if i == "" {
+		return nil, errors.MissingParam{Param: []string{"id"}}
+	}
+
+	id, err := strconv.Atoi(i)
+	if err != nil {
+		return nil, errors.InvalidParam{Param: []string{"id"}}
+	}
+
+	err = h.service.Delete(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &models.Response{
+		Message:    "deleted successfully",
+		StatusCode: http.StatusOK,
+	}, nil
 }
--- delivery/product/http_test.go
+++ delivery/product/http_test.go
@@ -1,6 +1,7 @@
 package product
 
 import (
+	"bytes"
 	"gofr-curd/models"
 	"gofr-curd/service"
 	"net/http"
@@ -25,16 +26,18 @@ func TestGetById(t *testing.T) {
 		desc   string
 		input  string
 		calls  []*gomock.Call
-		resp   *models.Product
+		resp   *models.Response
 		expErr error
 	}{
 		{
 			desc:  "success case",
 			input: "1",
-			resp: &models.Product{
-				Id:   1,
-				Name: "test",
-				Type: "example",
+			resp: &models.Response{
+				Data: models.Product{
+					Id:   1,
+					Name: "test",
+					Type: "example",
+				},
 			},
 			calls: []*gomock.Call{
 				mock.EXPECT().GetById(gomock.Any(), 1).Return(&models.Product{
@@ -113,5 +116,292 @@ func TestGetById(t *testing.T) {
 				t.Errorf("%s : expected %v, but got %v", tc.desc, tc.resp, resp)
 			}
 		})
+	}
+}
+
+func TestGet(t *testing.T) {
+	app := gofr.New()
+	ctrl := gomock.NewController(t)
+	mock := service.NewMockServices(ctrl)
+	h := New(mock)
+
+	testCases := []struct {
+		desc     string
+		mockCall []*gomock.Call
+		expResp  []*models.Product
+		expErr   error
+	}{
+		{
+			desc: "success",
+			mockCall: []*gomock.Call{
+				mock.EXPECT().Get(gomock.Any()).
+					Return([]*models.Product{
+						&models.Product{
+							Id:   1,
+							Name: "test",
+							Type: "example",
+						},
+						&models.Product{
+							Id:   2,
+							Name: "this",
+							Type: "that",
+						},
+					}, nil),
+			},
+			expResp: []*models.Product{
+				&models.Product{
+					Id:   1,
+					Name: "test",
+					Type: "example",
+				},
+				&models.Product{
+					Id:   2,
+					Name: "this",
+					Type: "that",
+				},
+			},
+			expErr: nil,
+		},
+		{
+			desc:    "error getting products",
+			expResp: nil,
+			expErr:  errors.EntityNotFound{Entity: "products", ID: "all"},
+			mockCall: []*gomock.Call{
+				mock.EXPECT().Get(gomock.Any()).Return(nil, errors.EntityNotFound{Entity: "products", ID: "all"}),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "http://dummy", nil)
+
+		req := request.NewHTTPRequest(r)
+		res := responder.NewContextualResponder(w, r)
+
+		ctx := gofr.NewContext(res, req, app)
+		resp, err := h.Get(ctx)
+		if !reflect.DeepEqual(err, tc.expErr) {
+			t.Errorf("%s : expected %v, but got %v", tc.desc, tc.expErr, err)
+		}
+		if tc.expErr == nil && !reflect.DeepEqual(resp, tc.expResp) {
+			t.Errorf("%s : expected %v, but got %v", tc.desc, tc.expResp, resp)
+		}
+	}
+}
+
+func TestCreate(t *testing.T) {
+	app := gofr.New()
+	ctrl := gomock.NewController(t)
+	mock := service.NewMockServices(ctrl)
+	h := New(mock)
+
+	testCases := []struct {
+		desc     string
+		input    []byte
+		expErr   error
+		mockCall []*gomock.Call
+	}{
+		{
+			desc:   "success",
+			input:  []byte(`{"name": "test","type": "example"}`),
+			expErr: nil,
+			mockCall: []*gomock.Call{
+				mock.EXPECT().Create(gomock.Any(), models.Product{
+					Name: "test",
+					Type: "example",
+				}).Return(&models.Product{
+					Id:   3,
+					Name: "test",
+					Type: "example",
+				}, nil),
+			},
+		},
+		{
+			desc:     "error binding",
+			input:    []byte(`{mock error invalid body}`),
+			mockCall: nil,
+			expErr:   errors.InvalidParam{Param: []string{"body"}},
+		},
+		{
+			desc:     "error invalid id",
+			input:    []byte(`{"id":1}`),
+			mockCall: nil,
+			expErr:   errors.InvalidParam{Param: []string{"id"}},
+		},
+		{
+			desc:  "error from service",
+			input: []byte(`{"name": "test","type": "example"}`),
+			mockCall: []*gomock.Call{
+				mock.EXPECT().Create(gomock.Any(), gomock.Any()).
+					Return(nil, errors.EntityAlreadyExists{}),
+			},
+			expErr: errors.EntityAlreadyExists{},
+		},
+	}
+
+	for _, tc := range testCases {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "http://dummy", bytes.NewReader(tc.input))
+
+		req := request.NewHTTPRequest(r)
+		res := responder.NewContextualResponder(w, r)
+
+		ctx := gofr.NewContext(res, req, app)
+		_, err := h.Create(ctx)
+		if !reflect.DeepEqual(err, tc.expErr) {
+			t.Errorf("%s : expected %v, but got %v", tc.desc, tc.expErr, err)
+		}
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	app := gofr.New()
+	ctrl := gomock.NewController(t)
+	mock := service.NewMockServices(ctrl)
+	h := New(mock)
+
+	testCases := []struct {
+		desc     string
+		id       string
+		input    []byte
+		expResp  *models.Product
+		expErr   error
+		mockCall []*gomock.Call
+	}{
+		{
+			desc:  "success",
+			id:    "1",
+			input: []byte(`{"name":"hello","type":"world"}`),
+			expResp: &models.Product{
+				Id:   1,
+				Name: "hello",
+				Type: "world",
+			},
+			expErr: nil,
+			mockCall: []*gomock.Call{
+				mock.EXPECT().Update(gomock.Any(), models.Product{
+					Id:   1,
+					Name: "hello",
+					Type: "world",
+				}).Return(&models.Product{
+					Id:   1,
+					Name: "hello",
+					Type: "world",
+				}, nil),
+			},
+		},
+		{
+			desc:   "missing params",
+			id:     "",
+			expErr: errors.MissingParam{Param: []string{"id"}},
+		},
+		{
+			desc:   "invalid params",
+			id:     "asd",
+			expErr: errors.InvalidParam{Param: []string{"id"}},
+		},
+		{
+			desc:   "binding err",
+			id:     "1",
+			input:  []byte(`mock error binding`),
+			expErr: errors.InvalidParam{Param: []string{"body"}},
+		},
+		{
+			desc:   "error in update",
+			id:     "1",
+			input:  []byte(`{"name":"hello","type":"world"}`),
+			expErr: errors.Error("error updating record"),
+			mockCall: []*gomock.Call{
+				mock.EXPECT().Update(gomock.Any(), models.Product{
+					Id:   1,
+					Name: "hello",
+					Type: "world",
+				}).Return(nil, errors.Error("error updating record")),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "http://dummy", bytes.NewReader(tc.input))
+
+		req := request.NewHTTPRequest(r)
+		res := responder.NewContextualResponder(w, r)
+
+		ctx := gofr.NewContext(res, req, app)
+		ctx.SetPathParams(map[string]string{
+			"id": tc.id,
+		})
+		_, err := h.Update(ctx)
+		if !reflect.DeepEqual(err, tc.expErr) {
+			t.Errorf("%s : expected %v, but got %v", tc.desc, tc.expErr, err)
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	app := gofr.New()
+	ctrl := gomock.NewController(t)
+	mock := service.NewMockServices(ctrl)
+	h := New(mock)
+
+	testCases := []struct {
+		desc     string
+		id       string
+		expErr   error
+		expResp  *models.Response
+		mockCall []*gomock.Call
+	}{
+		{
+			desc: "success",
+			id:   "1",
+			expResp: &models.Response{
+				Message:    "deleted successfully",
+				StatusCode: http.StatusOK,
+			},
+			mockCall: []*gomock.Call{
+				mock.EXPECT().Delete(gomock.Any(), 1).
+					Return(nil),
+			},
+		},
+		{
+			desc:   "missing params",
+			id:     "",
+			expErr: errors.MissingParam{Param: []string{"id"}},
+		},
+		{
+			desc:   "invalid params",
+			id:     "asd",
+			expErr: errors.InvalidParam{Param: []string{"id"}},
+		},
+		{
+			desc:   "error deleting records",
+			id:     "1",
+			expErr: errors.Error("error deleting record"),
+			mockCall: []*gomock.Call{
+				mock.EXPECT().Delete(gomock.Any(), 1).Return(errors.Error("error deleting record")),
+			},
+		},
+	}
+	for _, tc := range testCases {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "http://dummy", nil)
+
+		req := request.NewHTTPRequest(r)
+		res := responder.NewContextualResponder(w, r)
+
+		ctx := gofr.NewContext(res, req, app)
+		ctx.SetPathParams(map[string]string{
+			"id": tc.id,
+		})
+		resp, err := h.Delete(ctx)
+		if !reflect.DeepEqual(err, tc.expErr) {
+			t.Errorf("%s : expected %v, but got %v", tc.desc, tc.expErr, err)
+		}
+		if tc.expErr == nil && !reflect.DeepEqual(resp, tc.expResp) {
+			t.Errorf("%s : expected %v, but got %v", tc.desc, tc.expResp, resp)
+		}
+
 	}
 }
