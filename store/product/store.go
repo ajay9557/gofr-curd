f7439561b30acd84f92860b866f535411b6922f5
Vipul <vipulrawat008@gmail.com>
2022-02-07 09:54:16 +0530

    Adding store layer create, update and delete

--- store/product/store.go
+++ store/product/store.go
@@ -2,6 +2,7 @@ package product
 
 import (
 	"database/sql"
+	"github.com/go-sql-driver/mysql"
 	"gofr-curd/models"
 	"gofr-curd/store"
 	"strconv"
@@ -16,6 +17,31 @@ func New() store.Store {
 	return &product{}
 }
 
+func (p *product) Get(ctx *gofr.Context) ([]*models.Product, error) {
+	var res []*models.Product
+	rows, err := ctx.DB().QueryContext(ctx, "select id,name,type from products")
+	if err != nil {
+		return nil, errors.EntityNotFound{
+			Entity: "products",
+			ID:     "all",
+		}
+	}
+
+	for rows.Next() {
+		var p models.Product
+		err := rows.Scan(&p.Id, &p.Name, &p.Type)
+		if driverErr, ok := err.(*mysql.MySQLError); ok {
+			if driverErr.Number == 1045 {
+				return nil, errors.Error("Access denied")
+			} else {
+				return nil, errors.Error("Cannot scan row")
+			}
+		}
+		res = append(res, &p)
+	}
+	return res, nil
+}
+
 func (p *product) GetById(ctx *gofr.Context, id int) (*models.Product, error) {
 	var resp models.Product
 	resp.Id = id
@@ -26,4 +52,28 @@ func (p *product) GetById(ctx *gofr.Context, id int) (*models.Product, error) {
 	}
 
 	return &resp, nil
+}
+
+func (p *product) Create(ctx *gofr.Context, pd models.Product) error {
+	_, err := ctx.DB().ExecContext(ctx, "insert into products(id,name,type) values(?,?,?)", pd.Id, pd.Name, pd.Type)
+	if err != nil {
+		return errors.EntityAlreadyExists{}
+	}
+	return nil
+}
+
+func (p *product) Update(ctx *gofr.Context, pd models.Product) error {
+	_, err := ctx.DB().ExecContext(ctx, "update products set name=?, type=? where id=?", pd.Name, pd.Type, pd.Id)
+	if err != nil {
+		return errors.Error("error updating record")
+	}
+	return nil
+}
+
+func (p *product) Delete(ctx *gofr.Context, id int) error {
+	_, err := ctx.DB().ExecContext(ctx, "delete from products where id=?", id)
+	if err != nil {
+		return errors.Error("error deleting record")
+	}
+	return nil
 }
--- store/product/store_test.go
+++ store/product/store_test.go
@@ -3,51 +3,89 @@ package product
 import (
 	"context"
 	"gofr-curd/models"
-	"log"
 	"reflect"
 	"testing"
 
+	"developer.zopsmart.com/go/gofr/pkg/datastore"
 	"developer.zopsmart.com/go/gofr/pkg/errors"
 	"developer.zopsmart.com/go/gofr/pkg/gofr"
-	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/jinzhu/gorm"
 )
 
 func TestCoreLayer(t *testing.T) {
 	app := gofr.New()
-	db, mock, _ := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
-	database, err := gorm.Open("mysql", db)
-	if err != nil {
-		log.Println("Error opening gorm conn", db)
-	}
-	app.ORM = database
-	testGetById(t, app, mock)
+
+	seeder := datastore.NewSeeder(&app.DataStore, "../../db")
+	seeder.ResetCounter = true
+	//testGet(t, app)
+	seeder.RefreshTables(t, "products")
+	//testGetById(t, app)
+	testCreate(t, app)
+	//seeder.RefreshTables(t, "products")
 }
 
-func testGetById(t *testing.T, app *gofr.Gofr, mock sqlmock.Sqlmock) {
-
+func testGet(t *testing.T, app *gofr.Gofr) {
 	testCases := []struct {
-		desc      string
-		input     int
-		mockCalls []*sqlmock.ExpectedQuery
-		expOut    *models.Product
-		expErr    error
+		desc   string
+		expErr error
+		expOut []*models.Product
+	}{
+		{
+			desc:   "success case",
+			expErr: nil,
+			expOut: []*models.Product{
+				&models.Product{
+					Id:   1,
+					Name: "test",
+					Type: "example",
+				},
+				&models.Product{
+					Id:   2,
+					Name: "this",
+					Type: "that",
+				},
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			ctx := gofr.NewContext(nil, nil, app)
+			ctx.Context = context.Background()
+
+			store := New()
+			out, err := store.Get(ctx)
+			if !reflect.DeepEqual(err, tc.expErr) {
+				t.Errorf("%s : expected %v, but got %v", tc.desc, tc.expErr, err)
+			}
+			if tc.expErr == nil && !reflect.DeepEqual(out, tc.expOut) {
+				t.Errorf("%s : expected %v, but got %v", tc.desc, tc.expOut, out)
+			}
+		})
+	}
+}
+
+func testGetById(t *testing.T, app *gofr.Gofr) {
+	testCases := []struct {
+		desc   string
+		input  int
+		expErr error
+		expOut *models.Product
+		//mockCalls []*sqlmock.ExpectedQuery
 	}{
 		{
 			desc:   "success case",
 			input:  1,
 			expErr: nil,
-			mockCalls: []*sqlmock.ExpectedQuery{
-				mock.ExpectQuery("select name, type from products where id=?").
-					WithArgs(1).
-					WillReturnRows(sqlmock.NewRows([]string{"id", "name", "type"}).
-						AddRow(1, "test", "example")),
-			},
 			expOut: &models.Product{
 				Id:   1,
 				Name: "test",
 				Type: "example",
 			},
+			//mockCalls: []*sqlmock.ExpectedQuery{
+			//	mock.ExpectQuery("select name, type from products where id=?").
+			//		WithArgs(1).
+			//		WillReturnRows(sqlmock.NewRows([]string{"id", "name", "type"}).
+			//			AddRow(1, "ss", "example")),
+			//},
 		},
 		{
 			desc:  "entity not in database",
@@ -56,11 +94,11 @@ func testGetById(t *testing.T, app *gofr.Gofr, mock sqlmock.Sqlmock) {
 				Entity: "product",
 				ID:     "1022",
 			},
-			mockCalls: []*sqlmock.ExpectedQuery{
-				mock.ExpectQuery("select name, type from products where id=?").
-					WithArgs(1022).
-					WillReturnRows(sqlmock.NewRows([]string{"id", "name", "type"})),
-			},
+			//mockCalls: []*sqlmock.ExpectedQuery{
+			//	mock.ExpectQuery("select name, type from products where id=?").
+			//		WithArgs(1022).
+			//		WillReturnRows(sqlmock.NewRows([]string{"id", "name", "type"})),
+			//},
 		},
 	}
 
@@ -77,5 +115,46 @@ func testGetById(t *testing.T, app *gofr.Gofr, mock sqlmock.Sqlmock) {
 		if tc.expErr == nil && !reflect.DeepEqual(out, tc.expOut) {
 			t.Errorf("%s : expected %v, but got %v", tc.desc, tc.expOut, out)
 		}
+	}
+}
+
+func testCreate(t *testing.T, app *gofr.Gofr) {
+	tesCases := []struct {
+		desc   string
+		input  models.Product
+		expErr error
+	}{
+		{
+			desc: "success case",
+			input: models.Product{
+				Id:   3,
+				Name: "this",
+				Type: "that",
+			},
+			expErr: nil,
+		},
+		{
+			desc: "Entity already exists",
+			input: models.Product{
+				Id:   1,
+				Name: "test",
+				Type: "example",
+			},
+			expErr: errors.EntityAlreadyExists{},
+		},
+	}
+
+	for _, tc := range tesCases {
+		ctx := gofr.NewContext(nil, nil, app)
+		ctx.Context = context.Background()
+		store := New()
+		resp, err := store.Create(ctx, tc.input)
+		if !reflect.DeepEqual(err, tc.expErr) {
+			t.Errorf("%s : expected %v, but got %v", tc.desc, tc.expErr, err)
+		}
+		if tc.expErr == nil && !reflect.DeepEqual(resp, &tc.input) {
+			t.Errorf("%s : expected %v, but got %v", tc.desc, &tc.input, resp)
+		}
+
 	}
 }
